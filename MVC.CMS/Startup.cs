e925d66dd16945d69dda0d7d50ac6f6a71392719
gixLearning <38102077+gixLearning@users.noreply.github.com>
2018-06-11 15:49:21 +0200

    Initial commit

---
+++ MVC.CMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC.CMS.Startup))]
+namespace MVC.CMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
