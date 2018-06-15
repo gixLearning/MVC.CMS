6c692d65213b52d7989ead5e97dfc583519e81d4
gixLearning <38102077+gixLearning@users.noreply.github.com>
2018-06-15 15:37:42 +0200

    Working on requirements

--- MVC.CMS/App_Start/BundleConfig.cs
+++ MVC.CMS/App_Start/BundleConfig.cs
@@ -6,7 +6,9 @@ namespace MVC.CMS {
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles) {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.unobtrusive-ajax.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
---
+++ MVC.CMS/Models/ListUsersViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.CMS.Models {
+    public class ListUsersViewModel {
+        public IList<UserList> UserList { get; set; }
+    }
+
+    public class UserList {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+    }
+}
